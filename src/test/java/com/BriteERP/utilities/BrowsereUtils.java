c1163d05e0601a2db2e96a835962db6f5074a6d7
alpi <ruslanbaltabayev1983@gmail.com>
2019-08-06 00:08:47 -0700

    update

--- src/test/java/com/BriteERP/utilities/BrowsereUtils.java
+++ src/test/java/com/BriteERP/utilities/BrowsereUtils.java
@@ -1,4 +1,13 @@
 package com.BriteERP.utilities;
 
 public class BrowsereUtils {
+
+    public static void waitPlease(int seconds){
+        try {
+            Thread.sleep(seconds * 2000 );
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
